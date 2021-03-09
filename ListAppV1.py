c464238fb4bed09dd4b66ada9dea872cf05c1d54
BunnyQueen410 <78617194+BunnyQueen410@users.noreply.github.com>
2021-03-09 12:03:56 -0700

    Add files via upload

---
+++ ListAppV1.py
@@ -0,0 +1,25 @@
+"""
+Program Goals:
+Do the things
+"""
+
+#create functions that will perform those actions above
+
+def mainProgram():
+    myList = []
+    print("Hello, there! Lets work with some lists today")
+    print("Choose one of the following options. Type a number ONLY")
+    choice = input("1. Add to List, 2. Return the value at an index prosition.    ")
+    if choice == "1":
+        addToList()
+    elif choice == "2":
+        indexValues()
+
+def addToList():
+
+def indexValues():
+
+
+
+if __name__ == "__main__":
+    mainProgram()
